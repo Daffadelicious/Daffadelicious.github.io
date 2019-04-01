4917d7b1284fd626751eab8ea8b131f8aafb2db6
Daffadelicious <31669829+Daffadelicious@users.noreply.github.com>
2019-04-01 12:31:12 -0700

    fixed nav

--- acme/js/functions.js
+++ acme/js/functions.js
@@ -1,20 +1,11 @@
 // Functions for the ACME site
-/*
-    1. Navigation bar
-        a. Fill Navigation from json file
-        b. Detect whether nav is clicked
-        c. Call nav specific function to fill page 
-    2. Fill page with new information
-        a. Apply a hide class to the main page div
-        b. Show a status message ("Getting information")
-        c. Gather information and store in variables
-        d. Feed variables into html elements
-*/
 
 // Link to json file
 let dataURL = "/acme/js/acme.json";
+
 // Call fetchData function
-let data = fetchData(dataURL);
+fetchData(dataURL);
+
 function fetchData(dataURL){
     fetch(dataURL)
     .then(function(response) {
@@ -33,46 +24,50 @@ function fetchData(dataURL){
         for(let i=0; i<data.Navigation.name.length; i++){
             navItems[i] = data.Navigation.name[i];
         }
-
-        // Pass nav items to fillNav function
+        
+        // Call fillNav function
         fillNav(navItems);
-
-        // Return data
-        return data;
     })
 }
 
 function fillNav(navItems){
     let ul = document.getElementById("navUl");
-    let li = ul.getElementsByTagName("li");
-    for(let i = 0, j = 1; i < li.length; i++, j++){
-        li[j].innerHTML = navItems[i];
-    }
-    console.log(data.Navigation.name[0]);
+        // Create and fill nav items
+        for(let i = 0; i < navItems.length; i++){
+            let li = document.createElement("li");
+            let text = document.createTextNode(navItems[i]);
+            li.appendChild(text);
+            li.setAttribute("id", "nav" + navItems[i]);
+            ul.appendChild(li);
+        }
+    // Call clickListeners function
+    clickListeners(navItems);
 }
 
-// When Home is clicked, call clickHome() function
-document.getElementById("navHome").addEventListener("click", clickHome);
+function clickListeners(items){
+    // When Home is clicked, call clickHome() function
+    document.getElementById("navHome").addEventListener("click", clickHome);
 
-// When Anvils is clicked, call navClick() function and pass "1"
-let anvils = document.querySelector("#navAnvils");
-anvils.addEventListener("click", navClick);
-anvils.myParam = "Anvils";
+    // When Anvils is clicked, call navClick() function and pass "1"
+    let anvils = document.querySelector("#navAnvils");
+    anvils.addEventListener("click", navClick);
+    anvils.myParam = "Anvils";
 
-// When Explosives is clicked, call navClick() function and pass "2"
-let explosives = document.querySelector("#navExplosives");
-explosives.addEventListener("click", navClick); 
-explosives.myParam = "Explosives";
+    // When Explosives is clicked, call navClick() function and pass "2"
+    let explosives = document.querySelector("#navExplosives");
+    explosives.addEventListener("click", navClick); 
+    explosives.myParam = "Explosives";
 
-// When Decoys is clicked, call navClick() function and pass "3"
-let decoys = document.querySelector("#navDecoys");
-decoys.addEventListener("click", navClick);
-decoys.myParam = "Decoys";
+    // When Decoys is clicked, call navClick() function and pass "3"
+    let decoys = document.querySelector("#navDecoys");
+    decoys.addEventListener("click", navClick);
+    decoys.myParam = "Decoys";
 
-// When Traps is clicked, call navClick() function and pass "4"
-let traps = document.querySelector("#navTraps");
-traps.addEventListener("click", navClick);
-traps.myParam = "Traps";
+    // When Traps is clicked, call navClick() function and pass "4"
+    let traps = document.querySelector("#navTraps");
+    traps.addEventListener("click", navClick);
+    traps.myParam = "Traps";
+}
 
 // Functions for each nav clicked
 function clickHome(){
@@ -102,8 +97,6 @@ function navClick(event){
             // Set o to data.event's parameter passed from click event (Explosives, Traps, etc)
             let o = data[event.target.myParam];
             console.log("User clicked " + event.target.myParam);
-            console.log("Data passed:");
-            console.log(o);
 
             document.getElementById("itemHeader").innerHTML = o.name;
             document.getElementById("itemPicture").setAttribute("src", o.path);
